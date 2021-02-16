046bd00b8294a8f3ecb320ea4ec0aa6dfd1b9ae0
Loh Zhi Wei <zloh0008@student.monash.edu>
2021-02-16 16:56:21 +1100

    Add project files.

---
+++ MiniGames/MiniGames.h
@@ -0,0 +1,17 @@
+
+#ifndef MINIGAMES_H
+#define MINIGAMES_H
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <ctime>
+#include <sstream>
+
+using namespace std;
+
+#endif // !MINIGAMES_H
+
+
